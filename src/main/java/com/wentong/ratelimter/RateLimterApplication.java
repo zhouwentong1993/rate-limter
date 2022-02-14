679a3a63ae9d83214a93922afb5389046ea3e10b
zhouwentong <zhouwentong1993@gmail.com>
2022-02-14 10:06:38 +0800

    Initial commit

---
+++ src/main/java/com/wentong/ratelimter/RateLimterApplication.java
@@ -0,0 +1,13 @@
+package com.wentong.ratelimter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateLimterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wentong/ratelimter/RateLimterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wentong.ratelimter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
